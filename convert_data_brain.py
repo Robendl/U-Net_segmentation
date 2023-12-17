7ed3e4388e50857b67ac1e5645b31654ae5cf021
Roben <robendelange@gmail.com>
2023-12-17 15:18:04 +0100

    convert_data_brain cropped en resized images nu voordat die ze opslaat

--- convert_data_brain.py
+++ convert_data_brain.py
@@ -1,37 +1,53 @@
 import os
 import shutil
+
+import cv2
 import numpy as np
 
-# Define the total number of images
-total_images = 3064
+from create_mask import create_mask
 
-dir_train_imgs = "brain_tumour/train/images"
-dir_train_labels = "brain_tumour/train/masks"
-dir_test_imgs = "brain_tumour/test/images"
-dir_test_labels = "brain_tumour/test/masks"
 
-# Create directories if they don't exist
-os.makedirs(dir_train_imgs, exist_ok=True)
-os.makedirs(dir_train_labels, exist_ok=True)
-os.makedirs(dir_test_imgs, exist_ok=True)
-os.makedirs(dir_test_labels, exist_ok=True)
+def remove_black_space(img, label):
+    return
 
-# Generate random indices for 90% train and 10% test
-indices = np.arange(1, total_images + 1)
-np.random.shuffle(indices)
-train_size = int(total_images * 0.9)
-train_indices = indices[:train_size]
-test_indices = indices[train_size:]
 
-# Move images and masks based on the selected indices
-for new_idx, idx in enumerate(train_indices):
-    image_path = os.path.join(dir_train_imgs, str(new_idx) + ".png")
-    label_path = os.path.join(dir_train_labels, str(new_idx) + ".png")
-    shutil.copy(f'brain_tumour/images/{idx}.png', image_path)
-    shutil.copy(f'brain_tumour/masks/{idx}.png', label_path)
+def convert_data_brain():
+    # Define the total number of images
+    total_images = 3064
 
-for new_idx, idx in enumerate(test_indices):
-    image_path = os.path.join(dir_test_imgs, str(new_idx) + ".png")
-    label_path = os.path.join(dir_test_labels, str(new_idx) + ".png")
-    shutil.copy(f'brain_tumour/images/{idx}.png', image_path)
-    shutil.copy(f'brain_tumour/masks/{idx}.png', label_path)
+    dir_train_imgs = "brain_tumour/train/images"
+    dir_train_labels = "brain_tumour/train/masks"
+    dir_test_imgs = "brain_tumour/test/images"
+    dir_test_labels = "brain_tumour/test/masks"
+
+    # Create directories if they don't exist
+    os.makedirs(dir_train_imgs, exist_ok=True)
+    os.makedirs(dir_train_labels, exist_ok=True)
+    os.makedirs(dir_test_imgs, exist_ok=True)
+    os.makedirs(dir_test_labels, exist_ok=True)
+
+    # Generate random indices for 90% train and 10% test
+    indices = np.arange(1, total_images + 1)
+    np.random.shuffle(indices)
+    train_size = int(total_images * 0.9)
+    train_indices = indices[:train_size]
+    test_indices = indices[train_size:]
+
+    # Move images and masks based on the selected indices
+    for new_idx, idx in enumerate(train_indices):
+        image_path = os.path.join(dir_train_imgs, str(new_idx) + ".png")
+        label_path = os.path.join(dir_train_labels, str(new_idx) + ".png")
+        resized_image, resized_label = create_mask(f'brain_tumour/images/{idx}.png', f'brain_tumour/masks/{idx}.png')
+        cv2.imwrite(image_path, resized_image)
+        cv2.imwrite(label_path, resized_label)
+
+    for new_idx, idx in enumerate(test_indices):
+        image_path = os.path.join(dir_test_imgs, str(new_idx) + ".png")
+        label_path = os.path.join(dir_test_labels, str(new_idx) + ".png")
+        resized_image, resized_label = create_mask(f'brain_tumour/images/{idx}.png', f'brain_tumour/masks/{idx}.png')
+        cv2.imwrite(image_path, resized_image)
+        cv2.imwrite(label_path, resized_label)
+
+
+if __name__ == '__main__':
+    convert_data_brain()
--- create_mask.py
+++ create_mask.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import random as r
 
-def create_mask():
-    # Load the image (replace 'your_image.png' with the actual filename)
-    image = cv2.imread('data/train/tumor_images/image100.png', cv2.IMREAD_GRAYSCALE)
+
+def create_mask(image_path, label_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
 
     # Apply thresholding to create a binary mask
     _, thresholded = cv2.threshold(image, 70, 255, cv2.THRESH_BINARY)
@@ -13,41 +15,49 @@ def create_mask():
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
 
-    # Apply the mask to extract the lung area
-    result = cv2.bitwise_and(image, image, mask=mask)
+    height, width = np.shape(mask)
+    ranges = [range(width-1), range(width-1, 0, -1), range(height-1), range(height-1, 0, -1)]
+    sizes = [width, width, height, height]
+    edges = []
+    for j in range(len(sizes)):
+        loop_range = ranges[j]
+        size = sizes[j]
+        for i in loop_range:
+            if (j < 2 and np.sum(mask[i, :]) > 0.05) or np.sum(mask[:, i] > 0.05):
+                edges.append(i)
+                break
 
-    # Visualize the results
-    # cv2.imshow('Original Image', image)
-    # cv2.imshow('Lung Mask', mask)
-    # cv2.imshow('Result', result)
+    cropped_image = image[edges[0]:edges[1], edges[2]:edges[3]]
+    resized_image = cv2.resize(cropped_image, (width, height))
+
+    cropped_label = label[edges[0]:edges[1], edges[2]:edges[3]]
+    resized_label = cv2.resize(cropped_label, (width, height))
+
+    return resized_image, resized_label
+    # cv2.imshow("Original", image)
+    # cv2.imshow("Cropped", cropped_image)
+    # cv2.imshow("Resized", resized_image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    remove_columns = []
-
-    for i in range(0, mask.shape[1] - 1):
-        threshold = mask.shape[0] * 0.9
-        print(np.sum(mask[i]) == 0.0)
-        if 0.9 * np.sum(mask[i]) == 0.0 > 116000:
-            remove_columns.append(i)
-        
-    print(remove_columns)
-    exit()
-
-    # Remove columns 1 and 2 (indexing starts from 0)
-    new_arr = np.delete(mask, remove_columns, axis=1)
-
-    # Display the NumPy array as an image using Matplotlib
-    plt.imshow(new_arr, cmap='gray')  # 'cmap' sets the colormap (use 'gray' for grayscale)
-    plt.axis('off')  # Turn off axis labels and ticks
-    plt.show()
-
-    # Display the NumPy array as an image using Matplotlib
-    plt.imshow(mask, cmap='gray')  # 'cmap' sets the colormap (use 'gray' for grayscale)
-    plt.axis('off')  # Turn off axis labels and ticks
-    plt.show()
-
+    # image[edges[0], :] = 255
+    # image[edges[1], :] = 255
+    # image[:, edges[2]] = 255
+    # image[:, edges[3]] = 255
+    #
+    # mask[edges[0], :] = 255
+    # mask[edges[1], :] = 255
+    # mask[:, edges[2]] = 255
+    # mask[:, edges[3]] = 255
+    #
+    # plt.imshow(mask, cmap='gray')  # 'cmap' sets the colormap (use 'gray' for grayscale)
+    # plt.axis('off')  # Turn off axis labels and ticks
+    # plt.show()
 
 
 if __name__ == '__main__':
-    create_mask()
+    num_images = 3064
+    for _ in range(10):
+        img_idx = r.randint(1, 3065)
+        print(img_idx)
+        create_mask(f'brain_tumour/images/{img_idx}.png', f'brain_tumour/masks/{img_idx}.png')
