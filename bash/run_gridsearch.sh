8269a6930412da3e70c090622b1cac8df8c4a168
Roben <robendelange@gmail.com>
2024-01-21 13:52:15 +0100

    test

--- bash/run_gridsearch.sh
+++ bash/run_gridsearch.sh
@@ -8,15 +8,14 @@
 #SBATCH --job-name=gridsearch
 #SBATCH --output=gridsearch.out
 
+module purge
+module load Python/3.10.4-GCCcore-11.3.0
+module load OpenCV/4.6.0-foss-2022a-contrib
+module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
+module load scikit-learn/1.1.2-foss-2022a
+module load matplotlib/3.7.0-gfbf-2022b
+module load Pillow/9.4.0-GCCcore-12.2.0
 
 source /home3/s3799174/machinelearning/venv/bin/activate
-
-module load Python/3.10.4-GCCcore-11.3.0 --ignore_cache
-module load OpenCV/4.6.0-foss-2022a-contrib --ignore_cache
-module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0 --ignore_cache
-module load scikit-learn/1.1.2-foss-2022a --ignore_cache
-module load matplotlib/3.7.0-gfbf-2022b --ignore_cache
-module load Pillow/9.4.0-GCCcore-12.2.0 --ignore_cache
-
 
 python gridsearch.py
