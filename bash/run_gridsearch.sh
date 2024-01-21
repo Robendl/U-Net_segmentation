aade965fd4a1bf94b904fda18219bc175bb2edd0
Roben <robendelange@gmail.com>
2024-01-21 22:39:05 +0100

    3 day run

--- bash/run_gridsearch.sh
+++ bash/run_gridsearch.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --mem=32GB
-#SBATCH --job-name=test
-#SBATCH --output=test.out
+#SBATCH --job-name=gridsearch
+#SBATCH --output=gridsearch.out
 
 source /home3/s3799174/machinelearning/venv/bin/activate
 
