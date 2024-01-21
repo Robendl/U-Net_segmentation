eaaff177bdac5198ad158251a008daaa8abdc686
Roben <robendelange@gmail.com>
2024-01-21 13:55:55 +0100

    old bash

--- bash/run_gridsearch.sh
+++ bash/run_gridsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-3:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
@@ -8,7 +8,8 @@
 #SBATCH --job-name=gridsearch
 #SBATCH --output=gridsearch.out
 
-module purge
+source /home3/s3799174/machinelearning/venv/bin/activate
+
 module load Python/3.10.4-GCCcore-11.3.0
 module load OpenCV/4.6.0-foss-2022a-contrib
 module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
@@ -16,6 +17,5 @@ module load scikit-learn/1.1.2-foss-2022a
 module load matplotlib/3.7.0-gfbf-2022b
 module load Pillow/9.4.0-GCCcore-12.2.0
 
-source /home3/s3799174/machinelearning/venv/bin/activate
 
 python gridsearch.py
