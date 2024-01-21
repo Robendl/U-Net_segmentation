f51527dae35ff22b21895bb459be36336afade1a
Roben <robendelange@gmail.com>
2024-01-21 12:19:17 +0100

    lower batch size

--- gridsearch.py
+++ gridsearch.py
@@ -30,7 +30,7 @@ def gridsearch():
     # Create a hyperparameter grid to search
     param_grid = {
         'learning_rate': [0.001],
-        'batch_size': [24],
+        'batch_size': [20],
         'loss_function': [bce_loss]
     }
 
